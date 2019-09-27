5781a830ef3a3ab86510f35053998d6f343b2761
chicus12 <chicus12@gmail.com>
2019-09-27 02:49:19 -0600

    blabl

--- packages/2-static-test/lint.js
+++ packages/2-static-test/lint.js
@@ -1,12 +1,10 @@
-var name = "Freddy";
-typeof name === "string";
+var name = 'Freddy'
+typeof name === 'string'
 
-if (!"serviceWorker" in navigator) {
+if (!'serviceWorker' in navigator) {
   // you have an old browser :-(
 }
 
-var name = "hello"
-console
-  .log('${greeting} world!');
-
-[(1, 2, 3)].forEach(x => console.log(x))
+var name = 'hello'
+console.log('${greeting} world!')
+;[(1, 2, 3)].forEach(x => console.log(x))

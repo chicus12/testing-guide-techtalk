bbbc1d8e36dccc60f88738d73d2029631aa16821
chicus12 <chicus12@gmail.com>
2019-09-27 01:40:43 -0600

    se agrega metodos explicacion static

---
+++ packages/2-static-test/lint.js
@@ -0,0 +1,12 @@
+var name = "Freddy";
+typeof name === "string";
+
+if (!"serviceWorker" in navigator) {
+  // you have an old browser :-(
+}
+
+var greeting = "hello"
+console
+  .log('${greeting} world!');
+
+[(1, 2, 3)].forEach(x => console.log(x))
